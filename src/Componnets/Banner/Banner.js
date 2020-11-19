d0fa099ca575ef7b6a7c0e1de053241182b6aa31
unknown <masumbillah065@gmail.com>
2020-11-19 01:15:59 -0800

    porject base

---
+++ src/Componnets/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.scss';
+
+const Banner = () => {
+	return (
+		<div>
+			<h3>Hello From Banner</h3>
+		</div>
+	);
+};
+
+export default Banner;
---
+++ src/Componnets/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+	return <> This is header</>;
+};
+
+export default Header;
