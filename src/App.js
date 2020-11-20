9cdbfb68dfe25f82071266283ac97847980cc5c9
shakilmahammud <hshakil987@gmail.com>
2020-11-20 15:33:03 +0600

    working

--- src/App.js
+++ src/App.js
@@ -10,12 +10,18 @@ function App() {
 	return (
 		<>
 			<Header />
+<<<<<<< HEAD
 			<Services/>
      		 <Achivment />
 			<StayRunning/>
 			<Price/>
 			<Footer/>
 			
+=======
+			<StayRunning />
+			<Achivment />
+			<Footer />
+>>>>>>> 9101442d9a6b1f27d1e7b9c37cfcacc3ee17a33b
 		</>
 	);
 }
--- src/Componnets/Achivment/Achivment/Achivment.js
+++ src/Componnets/Achivment/Achivment/Achivment.js
@@ -2,12 +2,38 @@ import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import AchivmentCard from '../AchivmentCard/AchivmentCard';
 import './Achivment.scss';
+import { ReactComponent as Happy } from '../../../images/happy.svg';
+import { ReactComponent as Marketing } from '../../../images/marketing.svg';
+import { ReactComponent as Surface } from '../../../images/surface.svg';
+import { ReactComponent as Transportation } from '../../../images/transportation.svg';
 
 const Achivment = () => {
+	const achivments = [
+		{
+			title: 'Happy Clients',
+			count: 700,
+			icon: <Happy />,
+		},
+		{
+			title: 'Projects Completed',
+			count: 140,
+			icon: <Marketing />,
+		},
+		{
+			title: 'Crazy Minds',
+			count: 25,
+			icon: <Surface />,
+		},
+		{
+			title: 'Running Projects',
+			count: 75,
+			icon: <Transportation />,
+		},
+	];
 	return (
-		<Container>
+		<Container className='py-5'>
 			<Row>
-				<Col lg={4}>
+				<Col lg={5}>
 					<div className='achivment-title'>
 						<h2>Our Achievements</h2>
 						<p>
@@ -19,10 +45,11 @@ const Achivment = () => {
 						</p>
 					</div>
 				</Col>
-				<Col lg={8}>
+				<Col lg={7}>
 					<Row className='achivment-wraper'>
-						<AchivmentCard />
-						<AchivmentCard />
+						{achivments.map((achivment, index) => (
+							<AchivmentCard key={index} achivment={achivment} />
+						))}
 					</Row>
 				</Col>
 			</Row>
